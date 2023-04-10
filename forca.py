ce87877d3e00b3a9bbbde3807098d58e96e2a876
Diogospps <diogo.s.pedroso@gmail.com>
2023-04-10 20:08:15 -0300

    atualizando o jogo da forca

--- forca.py
+++ forca.py
@@ -5,53 +5,46 @@ def jogar():
     print("Bem vindo ao jogo da Forca!")
     print("*********************************")
 
-palavra = input("Digite a palavra secreta:").lower().strip()
-for x in range(100):
-    print()
-digitadas = []
-acertos = []
-erros = 0
-while True:
-    senha = ""
-    for letra in palavra:
-        senha += letra if letra in acertos else "."
-    print(senha)
-    if senha == palavra:
-        print("Você acertou!")
-        break
-    tentativa = input("\nDigite uma letra:").lower().strip()
-    if tentativa in digitadas:
-        print("Você já tentou esta letra!")
-        continue
-    else:
-        digitadas += tentativa
-        if tentativa in palavra:
-            acertos += tentativa
+    palavra_secreta = "banana".upper()
+    letras_acertadas = ["_" for letra in palavra_secreta]
+    
+
+    enforcou = False
+    acertou = False
+    tentativas = 0
+
+    print(letras_acertadas)
+
+    #enquanto nao enforcou e nao acertou
+    while(not enforcou and not acertou):
+        
+        chute = input("Qual a letra?")
+        chute = chute.strip().upper()
+        
+        if(chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if(chute.upper() == letra.upper()):
+                    letras_acertadas[index] = letra
+                index += 1
+                    
         else:
-            erros += 1
-            print("Você errou!")
-    print("X==:==\nX  :   ")
-    print("X  O   " if erros >= 1 else "X")
-    linha2 = ""
-    if erros == 2:
-        linha2 = "  |   "
-    elif erros == 3:
-        linha2 = " \|   "
-    elif erros >= 4:
-        linha2 = " \|/ "
-    print("X%s" % linha2)
-    linha3 = ""
-    if erros == 5:
-        linha3 += " /     "
-    elif erros >= 6:
-        linha3 += " / \ "
-    print("X%s" % linha3)
-    print("X\n===========")
-    if erros == 6:
-        print("Enforcado!")
-        break
+            tentativas += 1
 
+        enforcou == tentativas == 6
+        acertou = "_" not in letras_acertadas    
+        print(letras_acertadas)
 
+        if(acertou):
+            print("Você acertou todas as letras!!")
+        else:
+            print("Você não conseguiu acertar todas as letras!")
+    
+    
+    
+    
+    
+    
     print("Fim do jogo")
 
 
